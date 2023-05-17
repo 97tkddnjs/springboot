9cba31e1f1d440be1b2d6686549cfef0b939f968
97tkddnjs <tkddnjs206@gmail.com>
2023-05-17 10:38:33 +0900

    update advanced jpa

---
+++ advanced_jpa/src/main/java/com/springboot/advanced_jpa/data/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.springboot.advanced_jpa.data.repository;
+
+
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByName(String name, Sort sort);
+
+    Page<Product> findByName(String name, Pageable pageable);
+}
---
+++ advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.springboot.advanced_jpa.data.repository;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void sortingAndPagingTest() {
+        Product product1 = new Product();
+        product1.setName("pen");
+        product1.setPrice(1000);
+        product1.setStock(100);
+        product1.setCreatedAt(LocalDateTime.now());
+        product1.setUpdatedAt(LocalDateTime.now());
+        
+
+
+        Product product2 = new Product();
+        product2.setName("pen");
+        product2.setPrice(5000);
+        product2.setStock(300);
+        product2.setCreatedAt(LocalDateTime.now());
+        product2.setUpdatedAt(LocalDateTime.now());
+
+
+        Product product3 = new Product();
+        product3.setName("pen");
+        product3.setPrice(3000);
+        product3.setStock(200);
+        product3.setCreatedAt(LocalDateTime.now());
+        product3.setUpdatedAt(LocalDateTime.now());
+
+        Product saveProduct1 = productRepository.save(product1);
+        Product saveProduct2 = productRepository.save(product2);
+        Product saveProduct3 = productRepository.save(product3);
+
+
+        productRepository.findByName("pen", Sort.by(Sort.Order.asc("price")));
+        productRepository.findByName("pen", Sort.by(Sort.Order.asc("price"), Sort.Order.desc("stock")));
+
+        productRepository.findByName("pen", PageRequest.of(0,2));
+    }
+
+}
